80ce001c44c2d8a69f3d84226c0acd27c09c57da
Francis Tin-a <franfra10j@gmail.com>
2024-05-15 20:19:10 +0800

    added preprocessing for removing blank images in datasets

---
+++ remove_plain.py
@@ -0,0 +1,27 @@
+import os
+import cv2
+import numpy as np
+
+"""
+
+TODO: Remove plain black images from the dataset based on no pixel values
+
+Remain only images with pixel values in the same file path
+
+"""
+
+path = r"C:\Users\User\Desktop\HACK4HEALTH\HACKATHON_PRO\train\mask"
+
+def remove_black_images():
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+            print(file_path)
+            if img is None:
+                raise FileNotFoundError("Could not read the image file.")
+            if np.count_nonzero(img) == 0:
+                print("Removing:", file_path)
+                os.remove(file_path)
+
+remove_black_images()
